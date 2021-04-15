a9a85a12db5af0e3e2ce237056e5158eee550495
Andrew <81082984+daIuvenis@users.noreply.github.com>
2021-04-15 10:30:41 +0300

    Add files via upload

---
+++ src/main/java/singletonTempMail/singleton.java
@@ -0,0 +1,30 @@
+package singletonTempMail;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class singleton {
+    public static WebDriver driver = null;
+    public static String browserName = "chrome";
+
+    public static void initialize() {
+        if (driver == null) {
+            if(browserName.equalsIgnoreCase("chrome")){
+                System.setProperty("webdriver.chrome.driver","./chromedriver/chromedriver.exe");
+                driver = new ChromeDriver();
+            }
+        }
+        assert driver != null;
+        driver.manage().window().maximize();
+
+        singleton.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://tempmail.plus/ru/");
+    }
+
+    public static void quit(){
+        System.out.println("quitting the browser");
+        driver.quit();
+    }
+}
---
+++ src/main/java/tempMail.java
@@ -0,0 +1,238 @@
+
+import org.junit.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class tempMail {
+
+
+    public WebDriver driver;
+    public void ProfilePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(css = "#pre_rand")
+    private WebElement randomButton;
+
+    public void clickToButtonRandom(){
+        randomButton.click();
+    }
+
+    @FindBy(css = "#domain")
+    private WebElement dropDownMenu;
+
+    public void openDropDownMenu(){
+        dropDownMenu.click();
+    }
+
+    @FindBy(css = "#pre_button")
+    private WebElement inputNameAddress;
+
+    public String getNameAddress(){
+        String nameAddress = inputNameAddress.getAttribute("value");
+        String domainText = dropDownMenu.getAttribute("textContent");
+        return nameAddress + domainText;
+    }
+
+
+    @FindBy(css = "#pre_form > div > div.dropdown.mb-30.mb-md-0.show > div > button:nth-child(6)")
+    private WebElement roverInfo;
+
+    public void enterRoverInfo(){
+        roverInfo.click();
+    }
+
+    @FindBy(css = "#pre_settings")
+    private WebElement buttonSettings;
+
+    public void clickToButtonSetting(){
+        buttonSettings.click();
+    }
+
+    @FindBy(css = "#secret-address")
+    private WebElement textFieldSecretAddress;
+
+    public String getTextSecretAddress(){
+        String secretAddress = textFieldSecretAddress.getAttribute("textContent");
+        return secretAddress;
+    }
+
+    @FindBy(css = "#modal-settings > div > form > div.modal-header > div > button")
+    private WebElement buttonClose;
+
+    public void clickToButtonClose(){
+        buttonClose.click();
+    }
+
+    @FindBy(css = "#container-body > div > div.inbox > div > span")
+    private WebElement textWaitNewMessage;
+
+    public void getAssertTextInMenu(){
+        String waitMessageText = textWaitNewMessage.getAttribute("textContent");
+        Assert.assertEquals("В ожидании новых писем...",waitMessageText);
+    }
+
+    @FindBy(css = "#compose")
+    private WebElement buttonSend;
+
+    public void buttonComposeIsDisplayed(){
+        Assert.assertTrue(buttonSend.isDisplayed());
+    }
+    public void clickToButtonCompose(){
+    buttonSend.click();
+    }
+
+
+    @FindBy(css = "#to")
+    private WebElement sendTo;
+
+    public void setTextInTextFieldTo(){
+        sendTo.sendKeys(getNameAddress());
+    }
+
+    @FindBy(css = "#subject")
+    private WebElement textFieldTheme;
+
+    public void setTextInTextFieldSubject(){
+        textFieldTheme.sendKeys("Test");
+    }
+
+
+    @FindBy(css = "#text")
+    private WebElement textFieldTextMessage;
+
+    public void setTextInFieldText(){
+        textFieldTextMessage.sendKeys(getTextSecretAddress());
+    }
+    public void setSecondTextInFieldText(){
+        textFieldTextMessage.sendKeys("Test2");
+    }
+
+
+    @FindBy(css = "#submit")
+    private WebElement buttonSubmit;
+
+    public void clickToButtonSubmit(){
+        buttonSubmit.click();
+    }
+
+    @FindBy(css = "#container-body > div > div.inbox > div.mail > div")
+    private WebElement divMessage;
+
+    public void clickToDivMessage(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#container-body > div > div.inbox > div.mail > div")));
+        divMessage.click();
+    }
+
+    @FindBy(css = "#info > div.row.row-info.no-gutters > div.col.d-flex.mb-10 > span")
+    private WebElement emailSender;
+
+    public String getTextInFieldEmailSender(){
+        return emailSender.getAttribute("textContent");
+    }
+
+    @FindBy(css = "#info > div.subject.mb-20")
+    private WebElement textFieldThemeSender;
+
+    public String getTextInFieldThemeSender(){
+        return textFieldThemeSender.getAttribute("textContent");
+    }
+    public void assertThemeSender(){
+        Assert.assertEquals("Test", getTextInFieldEmailSender());
+    }
+
+    @FindBy(css = "#info > div.overflow-auto.mb-20")
+    private WebElement textSenderMessage;
+
+    public String getTextInFieldSenderMessage(){
+        return textSenderMessage.getAttribute("textContent");
+    }
+    public void assertSecretAddress(){
+        Assert.assertEquals(getTextSecretAddress(), getTextInFieldSenderMessage());
+    }
+
+    @FindBy(css = "#reply")
+    private WebElement buttonReply;
+
+    public void clickToButtonReply(){
+        buttonReply.click();
+    }
+
+    @FindBy(css = "#back")
+    private WebElement buttonBackToMenu;
+
+    public void clickToButtonBack(){
+        buttonBackToMenu.click();
+    }
+
+    @FindBy(css = "#container-body > div > div.inbox > div:nth-child(2) > div > div.subj.col-12.col-md-7.px-md-3 > span")
+    private WebElement textReTest;
+
+    public void clickToMessageText(){
+        textReTest.click();
+    }
+    public String getTextReTest(){
+        return textReTest.getAttribute("textContent");
+    }
+
+    public void assertReTest(){
+        Assert.assertEquals("Re: Test", getTextReTest());
+    }
+
+    @FindBy(css = "#info > div.overflow-auto.mb-20")
+    private WebElement textMessageSenderSecond;
+
+    public void assertTestSecond(){
+        String textMessageTest2 = textMessageSenderSecond.getAttribute("textContent");
+        Assert.assertEquals("Test2", textMessageTest2);
+    }
+
+    @FindBy(css = "#delete_mail")
+    private WebElement buttonDelete;
+
+    public void clickToButtonDelete(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#delete")));
+        buttonDelete.click();
+    }
+
+    @FindBy(css = "#confirm_mail")
+    private WebElement buttonConfirmDelete;
+
+    public void clickToButtonConfirm(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#confirm_mail")));
+        buttonConfirmDelete.click();
+    }
+    public void findElementRe(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#delete")));
+        Assert.assertFalse(driver.getPageSource().contains("Re: Test"));
+    }
+
+    public void settingsQuit(){
+        driver.quit();
+    }
+    public void settingsOpenBefore(){
+        System.setProperty("webdriver.chrome.driver", "./chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://tempmail.plus/ru/#!");
+    }
+
+
+}
